46d80b4f0e7ec8f82f823fed40a6d584491930e8
Ivan <ivanbrazas@gmail.com>
2022-10-19 09:12:29 -0300

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App h-full relative">
+      <div className="background h-2/6 w-full -z-30 absolute bg-blue-500"></div>
+
+      <h3>Hola</h3>
+    </div>
+  );
+}
+
+export default App;
