13685089154b39f1c8223fa82e0889af25744b3d
Ivan <ivanbrazas@gmail.com>
2022-10-20 18:20:02 -0300

    Circular progress bar done

--- frontend/src/components/Control.jsx
+++ frontend/src/components/Control.jsx
@@ -3,11 +3,14 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import controlSlice, { getGastos } from "../slices/control/controlSlice";
 import { formatCurrency } from "../helpers/formatCurrency";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 const Control = () => {
   const dispatch = useDispatch(controlSlice);
   const presupuesto = useSelector((state) => state.control.presupuesto);
   const gastos = useSelector((state) => state.control.gastos);
+  const [percentage, setPercentage] = useState(0);
   const [gastado, setGastado] = useState(0);
 
   useEffect(() => {
@@ -20,6 +23,11 @@ const Control = () => {
         (total, gasto) => Number(gasto.cantidad) + total,
         0
       );
+
+      const per = ((gas * 100) / presupuesto).toFixed(2);
+      console.log(per);
+      setPercentage(per);
+
       setGastado(gas);
     };
 
@@ -28,7 +36,18 @@ const Control = () => {
 
   return (
     <section className="  bg-white mt-10 rounded-lg shadow-md  px-10 py-6 text-center flex">
-      <div className="img-container w-1/2"></div>
+      <div className="img-container w-1/2">
+        <CircularProgressbar
+          className="w-10/12 mx-auto"
+          value={percentage}
+          text={`${percentage}%`}
+          styles={buildStyles({
+            pathTransitionDuration: 1,
+            pathColor: percentage < 100 ? "#60a5fa" : "#dc2626",
+          })}
+        />
+        ;
+      </div>
       <div className="control__container w-1/2">
         <button className="bg-red-500 text-white uppercase font-semibold w-full text-xs py-1 rounded-md ">
           Resetear app
